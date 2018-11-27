4217e0e48b37b4f71707ffb901330f3cff4b3fde
Luis David Betances M <david_bm42@hotmail.com>
2018-11-27 17:39:43 -0400

    .

--- Sistema de Venta/Reportes/FRM_ReporteVenta.cs
+++ Sistema de Venta/Reportes/FRM_ReporteVenta.cs
@@ -3,21 +3,25 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Reporting.WinForms;
 
 namespace Sistema_de_Venta
 {
     public partial class FRM_ReporteVenta : Form
     {
+        string nombreReporte, startupPath, tipoCliente;
+        int ventaId;
+
         public FRM_ReporteVenta()
         {
             InitializeComponent();
         }
-
 
         [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
         private extern static void ReleaseCapture();
@@ -25,18 +29,12 @@ namespace Sistema_de_Venta
         private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
 
 
-        public void SetVentaId(int ventaId)
+        public void SetVentaId_y_TipoCliente(int ventaId, string tipoCliente)
         {
-            txtVentaId.Text = ventaId.ToString();
-        } 
-
-        private void FRM_ReporteVenta_Load(object sender, EventArgs e)
-        {
-            // TODO: esta línea de código carga datos en la tabla 'DataSet2.usp_Reportes_GenrerarReporteVenta' Puede moverla o quitarla según sea necesario.
-            this.usp_Reportes_GenrerarReporteVentaTableAdapter.Fill(this.DataSet2.usp_Reportes_GenrerarReporteVenta, Convert.ToInt32(txtVentaId.Text));
-
-            this.reportViewer1.RefreshReport();
+            this.ventaId = ventaId;
+            this.tipoCliente = tipoCliente; 
         }
+
 
         private void btnClose_Click(object sender, EventArgs e)
         {
@@ -48,11 +46,30 @@ namespace Sistema_de_Venta
             this.WindowState = FormWindowState.Minimized;
         }
 
+        private void FRM_ReporteVenta_Load(object sender, EventArgs e)
+        {
+            if (tipoCliente == "Consumidor Final")
+            {
+                nombreReporte = "RptReporteVenta.rdlc";
+            }
+            else if (tipoCliente == "Crédito Fiscal")
+            {
+                nombreReporte = "RptReporteVentaCFiscal.rdlc";
+            }
+
+            startupPath = Path.Combine(Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.FullName, "Reportes\\" + nombreReporte);
+            this.reportViewer2.LocalReport.ReportPath = startupPath;
+
+            // TODO: esta línea de código carga datos en la tabla 'dbSFacturacionDataSet.usp_Reportes_GenrerarReporteVenta' Puede moverla o quitarla según sea necesario.
+            this.usp_Reportes_GenrerarReporteVentaTableAdapter.Fill(this.dbSFacturacionDataSet.usp_Reportes_GenrerarReporteVenta, ventaId);
+            this.reportViewer2.RefreshReport();
+
+        }
+
         private void pnl_factura_MouseDown(object sender, MouseEventArgs e)
         {
             ReleaseCapture();
             SendMessage(this.Handle, 0x112, 0xf012, 0);
         }
-
     }
 }
