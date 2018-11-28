67eacc34640aa2d22e411613255b0a0cd0565771
Luis David Betances M <david_bm42@hotmail.com>
2018-11-28 02:06:31 -0400

    Mejoras finales a el gui de las facturas

--- Sistema de Venta/Reportes/FRM_ReporteVenta.Designer.cs
+++ Sistema de Venta/Reportes/FRM_ReporteVenta.Designer.cs
@@ -66,7 +66,7 @@
             this.reportViewer1.Location = new System.Drawing.Point(0, 48);
             this.reportViewer1.Name = "reportViewer1";
             this.reportViewer1.ServerReport.BearerToken = null;
-            this.reportViewer1.Size = new System.Drawing.Size(805, 785);
+            this.reportViewer1.Size = new System.Drawing.Size(825, 785);
             this.reportViewer1.TabIndex = 0;
             // 
             // usp_Reportes_GenrerarReporteVentaTableAdapter
@@ -83,7 +83,7 @@
             this.pnl_factura.Location = new System.Drawing.Point(0, 0);
             this.pnl_factura.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pnl_factura.Name = "pnl_factura";
-            this.pnl_factura.Size = new System.Drawing.Size(805, 48);
+            this.pnl_factura.Size = new System.Drawing.Size(825, 48);
             this.pnl_factura.TabIndex = 2;
             this.pnl_factura.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pnl_factura_MouseDown);
             // 
@@ -93,7 +93,7 @@
             this.btnClose.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(56)))), ((int)(((byte)(117)))));
             this.btnClose.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnClose.Image = global::Sistema_de_Venta.Properties.Resources.close_white;
-            this.btnClose.Location = new System.Drawing.Point(752, 6);
+            this.btnClose.Location = new System.Drawing.Point(772, 6);
             this.btnClose.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnClose.Name = "btnClose";
             this.btnClose.Size = new System.Drawing.Size(42, 37);
@@ -108,7 +108,7 @@
             this.btnMinimize.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(56)))), ((int)(((byte)(117)))));
             this.btnMinimize.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnMinimize.Image = global::Sistema_de_Venta.Properties.Resources.minimize_white;
-            this.btnMinimize.Location = new System.Drawing.Point(707, 6);
+            this.btnMinimize.Location = new System.Drawing.Point(727, 6);
             this.btnMinimize.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnMinimize.Name = "btnMinimize";
             this.btnMinimize.Size = new System.Drawing.Size(42, 37);
@@ -122,7 +122,7 @@
             this.lbl_Login.AutoSize = true;
             this.lbl_Login.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbl_Login.ForeColor = System.Drawing.Color.White;
-            this.lbl_Login.Location = new System.Drawing.Point(324, 5);
+            this.lbl_Login.Location = new System.Drawing.Point(326, 5);
             this.lbl_Login.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbl_Login.Name = "lbl_Login";
             this.lbl_Login.Size = new System.Drawing.Size(173, 38);
@@ -133,12 +133,12 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.ClientSize = new System.Drawing.Size(805, 833);
+            this.ClientSize = new System.Drawing.Size(825, 833);
             this.Controls.Add(this.reportViewer1);
             this.Controls.Add(this.pnl_factura);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MinimumSize = new System.Drawing.Size(805, 500);
+            this.MinimumSize = new System.Drawing.Size(825, 500);
             this.Name = "FRM_ReporteVenta";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Factura";
